d14000f92e5ee12fe0d05bfe2300d3187cf231e2
WesleyAugusto <ranzerap@gmail.com>
2022-10-10 20:08:13 -0300

    loading mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,22 @@
 import { Background } from './src/components/Background';
 import { StatusBar } from 'react-native';
+import { Home } from './src/screens/Home';
+import {
+   useFonts,
+   Inter_400Regular,
+   Inter_600SemiBold,
+   Inter_700Bold,
+   Inter_900Black
+ } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+
+  });
   return (
     <Background>
     <StatusBar
@@ -10,6 +26,7 @@ export default function App() {
     translucent
 
     />
+    { fontsLoaded ? < Home /> : <Loading />}
     </Background>
   );
 }
