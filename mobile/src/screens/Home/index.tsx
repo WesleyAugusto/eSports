e116399a928413cc411847c83fc903da4d5bc6da
WesleyAugusto <ranzerap@gmail.com>
2022-10-18 21:01:13 -0300

    screen Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View } from 'react-native';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { View , Image, FlatList} from 'react-native';
 
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+        <Image source={logoImg} style={styles.logo}/>
+        <Heading title="Encontre Seu Duo!" subtitle="Seleciona Um Game Que Deseja Jogar..."/>
+        <FlatList data={GAMES} keyExtractor={item => item.id} renderItem={({ item }) => (
+          <GameCard data={item}/>
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+        />
+        
     </View>
   );
 }
