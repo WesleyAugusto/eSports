942fbebe0a1cc8e446bf121652af9fd09d846ded
WesleyAugusto <ranzerap@gmail.com>
2022-10-20 21:03:33 -0300

    back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games',(request, response) => {
+    return response.json([])
+})
+
+app.post('/ads',(request, response) => {
+    return response.json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+
+//const gameId = request.params.id;
    return response.json(
     
     [
@@ -17,4 +27,16 @@ app.get('/ads', (request, response) => {
    )
 });
 
+app.get('/ads/:id/discord', (request, response) => {
+
+    //const adId = request.params.id;
+       return response.json(
+        
+        [
+          
+        ],
+        
+        
+       )
+    });
 app.listen(3333)
